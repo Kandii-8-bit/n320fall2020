c22674452d72e159d29b4b8b9ea905f9c475a873
Kandii-8-bit <70590581+Kandii-8-bit@users.noreply.github.com>
2020-09-14 11:18:25 -0400

    Lab Start

---
+++ labs/lab 2/app.js
@@ -0,0 +1,9 @@
+// Instrument class or Parent
+class Instrument {
+    constructor(loudness, family, playverb) {
+        this.loudness = loudness;
+        this.family = family;
+        this.playverb = playverb;
+    }
+}
+
