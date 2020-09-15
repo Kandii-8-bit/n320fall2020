1499ef3b22b68b87923ef96a44d1852c55741b81
Kandii-8-bit <70590581+Kandii-8-bit@users.noreply.github.com>
2020-09-15 17:47:52 -0400

    Series of tests ran

--- labs/lab2/app.js
+++ labs/lab2/app.js
@@ -1,29 +1,42 @@
 // Instrument class or Parent
 class Instrument {
-    constructor(loudness, family, playverb) {
+    constructor(playverb, family, loudness) {
         this.loudness = loudness;
         this.family = family;
         this.playverb = playverb;
     }
 }
-console.log(Instrument);
+// console.log(Instrument);
 
 //Child classes for different instruments
+//Woodwind
+//Percussion
+//String
 class Woodwind extends Instrument {
-    constructor(playverb, family)
-    super(playverb, family, veryloud);
+     constructor(playverb, family) {
+        super(playverb, family, "veryloud");
+    }
+   
 }
 
 class Percussion extends Instrument {
-    constructor(playverb, family)
-    super(playverb, family, extremelyloud)
+     constructor(playverb, family) {
+        super(playverb, family, "extremelyloud")
+    }
+    
 }
 
 class String extends Instrument {
-    constructor(playverb, family)
-    super(playverb, family, soft)
+      constructor(playverb, family){
+        super(playverb, family, "soft")
+    }
+    
 }
 
 let myWoodwind = new Woodwind("Toots", "Piccolo", )
-console.log(Woodwind);
+console.log(myWoodwind);
+let myPercussion = new Percussion("Hit", "Snare Drum", )
+console.log(myPercussion);
+let myString = new String("Strum", "Acoustic Guitar", )
+console.log(myString);
 
