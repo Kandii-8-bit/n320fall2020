976a5a62f8394be16774ab7f3a4d17ae59b853a3
Kandii-8-bit <70590581+Kandii-8-bit@users.noreply.github.com>
2020-09-30 14:18:18 -0400

    Base of Project
    Got the board itself drawn out! The pieces are also drawn out as well. All pieces of boardgame are logged to console.

---
+++ labs/lab4/app.js
@@ -0,0 +1,23 @@
+let board = document.getElementById("board");
+console.log(board);
+
+let lineOne = document.getElementById("lineOne");
+console.log(lineOne);
+
+let lineTwo = document.getElementById("lineTwo");
+console.log(lineTwo);
+
+let lineThree = document.getElementById("lineThree");
+console.log(lineThree);
+
+let lineFour = document.getElementById("lineFour");
+console.log(lineFour);
+
+let gpOne = document.getElementById("gpOne");
+console.log(gpOne);
+
+let gpTwo = document.getElementById("gpTwo");
+console.log(gpTwo);
+
+let gpThree = document.getElementById("gpThree");
+console.log(gpThree);
