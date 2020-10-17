98880ef54ba3659359d9011e4b1f8e299faf3372
Kandii-8-bit <70590581+Kandii-8-bit@users.noreply.github.com>
2020-10-16 23:11:26 -0400

    Vue.js Lab Created

---
+++ labs/lab6/app.js
@@ -0,0 +1,15 @@
+let app = new Vue ({
+    el: "#app",
+    data: {
+        message: "Book One",
+        books: ["FFXV Dawn of The Future","⭐","Jun Eishima"]
+    }
+})
+
+let appTwo = new Vue ({
+    el: "#appTwo",
+    data: {
+        message: "Book Two",
+        books: ["Misery","🔪","Stephen King"]
+    }
+})
